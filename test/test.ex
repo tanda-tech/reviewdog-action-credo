ba1a0f925f0ea6a292536fb96de9e0318b69a6a8
Neel Mehta <neel@mytanda.com>
2025-09-26 16:02:10 -0400

    Clean up test code, keep working configuration
    - Remove syntax_error.ex test file
    - Clean up test.ex to remove violation code
    - Keep workflow at level: info since the fix is now proven to work
    - The -fail-level flag fix is working correctly

--- test/test.ex
+++ test/test.ex
@@ -1,26 +1,7 @@
 defmodule Foo do
-  # Missing @moduledoc - should be error level with strict mode
-
   def foo do
     bar(:atom)
     |> baz()
-  end
-
-  def unused_enum_operation do
-    # This should be a WARNING level issue
-    [1, 2, 3] |> Enum.map(&(&1 * 2))
-    :ok
-  end
-
-  def with_debug_code do
-    result = calculate_something()
-    # This should trigger Warning.IoInspect
-    IO.inspect(result)
-    result
-  end
-
-  defp calculate_something do
-    42
   end
 
   defp bar(atom), do: atom
